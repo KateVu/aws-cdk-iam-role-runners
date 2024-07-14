f373b652407a2ea1051181b3c9591accadad0dfa
KateVu <tuquyen87@gmail.com>
2024-07-15 01:50:51 +1000

    add create iam role for github runners

---
+++ bin/index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { IamRoleRunnersStack } from '../lib/iam';
+import { getAccountId } from '../lib/utils';
+
+//Environment variable for yaml file path and file name
+const configFolder = '../config/'
+const accountFileName = 'aws_account.yaml'
+
+//Set up default value
+const envName = process.env.ENVIRONMENT_NAME || 'kate'
+const accountName = process.env.ACCOUNT_NAME || 'sandpit1'
+const region = process.env.REGION || 'ap-southeast-2'
+
+//Get aws account id
+const accountId = getAccountId(accountName, configFolder, accountFileName)
+
+const app = new cdk.App();
+
+const iamStack = new IamRoleRunnersStack(app, 'IamRoleRunnersStack', {
+  stackName: `iam-role-runners-${envName}`,
+  region: region,
+  accountId: accountId,
+  accountName: accountName,
+  envName: envName,
+})
+
+cdk.Tags.of(iamStack).add('createdby', 'KateVu')
+cdk.Tags.of(iamStack).add('createdvia', 'AWS-CDK')
+cdk.Tags.of(iamStack).add('environment', envName)
+cdk.Tags.of(iamStack).add('repo', 'https://github.com/KateVu/aws-cdk-iam-role-runners')
+cdk.Tags.of(iamStack).add('DONOTDELETE', 'true')
---
+++ lib/iam.ts
@@ -0,0 +1,46 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+interface IamRoleRunnersStackPros extends StackProps {
+  region: string,
+  accountId: string,
+  accountName: string,
+  envName: string,
+}
+
+export class IamRoleRunnersStack extends Stack {
+  constructor(scope: Construct, id: string, props: IamRoleRunnersStackPros) {
+    const { region, accountId, accountName } = props
+    const updatedProps = {
+        env: {
+            region: region,
+            account: accountId,
+        },
+        ...props
+    }
+    super(scope, id, updatedProps)    
+
+    const githubOidcProvider = new iam.OpenIdConnectProvider(this, `GithubOidcProvider`, {
+      url: 'https://token.actions.githubusercontent.com',
+      thumbprints: ['1c58a3a8518e8759bf075b76b750d4f2df264fcd'],
+      clientIds: ['sts.amazonaws.com'],
+    })
+
+    const githubActionsDeploymentRole = new iam.Role(this, `GithubActionsDeploymentUser`, {
+      assumedBy: new iam.WebIdentityPrincipal(
+        githubOidcProvider.openIdConnectProviderArn,
+        {
+          StringLike: { 
+            'token.actions.githubusercontent.com:sub': 'repo:KateVu/aws-cdk*',
+            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+          },            
+        },
+      ),
+      roleName: 'github-actions-deployment-role',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+      ],      
+    })    
+  }
+}
